c2c16ff7cea8f6b74975053de3a01ed3168ea5f4
957jsh0551 <jsh0551@957.co.kr>
2024-04-26 18:50:21 +0900

    regression model update

--- ros_ws/customModule/DRL_model.py
+++ ros_ws/customModule/DRL_model.py
@@ -1,124 +1,56 @@
-# coding=utf-8
-# ----tuzixini@gmail.com----
-# WIN10 Python3.6.6
-# 用途: DRL_Lane Pytorch 实现
-# model.py
-
-import torch.nn as nn
-import pdb
+import random
+import numpy as np
+import cv2
 import torch
-import torch.nn.functional as F
-import math
-from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights
-from torchvision.models import mobilenet_v3_large, MobileNet_V3_Large_Weights
+import torch.nn as nn
+from config import cfg
+import os
+from torchvision.models import efficientnet_v2_m, EfficientNet_V2_M_Weights, efficientnet_v2_s, EfficientNet_V2_S_Weights,\
+MobileNet_V3_Large_Weights, mobilenet_v3_large
 
-class DRL_LANE(nn.Module):
-    def __init__(self, cfg):
-        super(DRL_LANE,self).__init__()
-        # 使用默认的strid和padding
-        self.landmark_num = cfg.LANDMARK_NUM
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 48, kernel_size=3),
-            nn.ReLU(),
-            nn.Conv2d(48, 96, kernel_size=3),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(96, 128, kernel_size=2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(128, 192, kernel_size=2),
-            nn.ReLU(),
-            nn.Conv2d(192, 256, kernel_size=2),
-            nn.ReLU())
-        
-        self.compress = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=1),
-            nn.ReLU())
-
-        qhead_list = []
-        for _ in range(self.landmark_num):
-            qhead_list.append(QHead(56448))
-        self.qhead_list = nn.ModuleList(qhead_list)
-
-    def forward(self, img, states):
-        img = self.encoder(img)
-        img = self.compress(img)
-        img = img.view(img.shape[0],-1)
-        qvals = []
-        for i in range(self.landmark_num):
-            qhead = self.qhead_list[i]
-            state = states[:,i,...]
-            q = qhead(img, state)
-            qvals.append(q)
-        qvals = torch.stack(qvals,dim=1)
-        return qvals
-
-
-class Mobilenet3_DRL(nn.Module):
-    def __init__(self, cfg, size = 'large'):
-        super(Mobilenet3_DRL,self).__init__()
-        self.landmark_num = cfg.LANDMARK_NUM
-        if size == 'small':
-            weights = MobileNet_V3_Small_Weights.DEFAULT
-            model = mobilenet_v3_small(weights=weights)
-            node_num = 576
-        else:
-            weights = MobileNet_V3_Large_Weights.DEFAULT
-            model = mobilenet_v3_large(weights=weights)
-            node_num = 960
+class Ghostnet_reg(nn.Module):
+    def __init__(self):
+        super(Ghostnet_reg, self).__init__()
+        model = torch.hub.load('huawei-noah/ghostnet', 'ghostnet_1x', pretrained=True)
         self.model = nn.Sequential(*list(model.children())[:-1])
-        qhead_list = []
-        for _ in range(self.landmark_num):
-            qhead_list.append(QHead(node_num))
-        self.qhead_list = nn.ModuleList(qhead_list)
-    def forward(self, x, states):
+        self.reg_layer = nn.Sequential(nn.Dropout(0.3),nn.Linear(1280,5))
+    def forward(self, x):
         img = self.model(x)
         img = img.view(img.size(0), -1)
-        qvals = []
-        for i in range(self.landmark_num):
-            qhead = self.qhead_list[i]
-            state = states[:,i,...]
-            q = qhead(img, state)
-            qvals.append(q)
-        qvals = torch.stack(qvals,dim=1)
-        return qvals
+        reg_value = self.reg_layer(img)
+        return reg_value
+
+class Efficient_V2_reg(nn.Module):
+    def __init__(self, size='medium'):
+        super(Efficient_V2_reg, self).__init__()
+        if size=='small':
+            weights = EfficientNet_V2_S_Weights.DEFAULT
+            model = efficientnet_v2_s(weights=weights)
+        else:
+            weights = EfficientNet_V2_M_Weights.DEFAULT
+            model = efficientnet_v2_m(weights=weights)
+        self.model = nn.Sequential(*list(model.children())[:-1])
+        self.reg_layer = nn.Sequential(nn.Dropout(0.3),nn.Linear(1280,5))
+    def forward(self, x):
+        img = self.model(x)
+        img = img.view(img.size(0), -1)
+        reg_value = self.reg_layer(img)
+        return reg_value
     
+class MobilenetV3_reg(nn.Module):
+    def __init__(self):
+        super(MobilenetV3_reg, self).__init__()
+        weights = MobileNet_V3_Large_Weights.DEFAULT
+        model = mobilenet_v3_large(weights=weights)
+        self.model = nn.Sequential(*list(model.children())[:-1])
+        self.reg_layer = nn.Sequential(nn.Dropout(0.3),nn.Linear(960,5))
+    def forward(self, x):
+        img = self.model(x)
+        img = img.view(img.size(0), -1)
+        reg_value = self.reg_layer(img)
+        return reg_value  
 
-# class QHead(nn.Module):
-#     def __init__(self, node_num):
-#         super(QHead, self).__init__()
-#         self.fc1 = nn.Sequential(
-#             nn.Linear(node_num, 512),
-#             nn.ReLU())
-#         self.fc2 = nn.Sequential(
-#             nn.Linear(545, 256),
-#             nn.ReLU())
-#         self.fc3 = nn.Sequential(
-#             nn.Linear(256, 4))
-
-#     def forward(self, img, state):
-#         img = self.fc1(img)
-#         img = torch.cat((img, state),1)
-#         img = self.fc2(img)
-#         img = self.fc3(img)
-#         return img
-
-class QHead(nn.Module):
-    def __init__(self, node_num):
-        super(QHead, self).__init__()
-        self.fc1 = nn.Sequential(
-            nn.Linear(node_num+33, 256),
-            nn.ReLU())
-        self.fc2 = nn.Sequential(
-            nn.Linear(256, 4))
-
-    def forward(self, img, state):
-        img = torch.cat((img, state),1)
-        img = self.fc1(img)
-        img = self.fc2(img)
-        return img
-
-def getModel(cfg):
+def getModel():
 #     net = DRL_LANE(cfg)
-    net = Mobilenet3_DRL(cfg)
+    net = MobilenetV3_reg()
     return net
